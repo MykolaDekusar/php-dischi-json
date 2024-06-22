4680c55b21217be32a1efa6d74b61097b6508b1d
MykolaDekusar <ndekusar97@gmail.com>
2024-06-22 02:20:50 +0200

    [UPD] Added glass style, added modal popup for each card click.

--- index.php
+++ index.php
@@ -11,33 +11,51 @@
 
 <body>
     <div id="app" v-cloak>
-        <header>
-            <h1>{{title}}</h1>
-        </header>
-        <main>
-            <div class="container">
-                <div class="row">
-                    <ul>
-                        <li v-for="card in data">
-                            <div class="card">
-                                <div class="img-cont">
-                                    <img :src="card.cover" alt="">
+        <div :class="{overflow: !hide}">
+            <header>
+                <h1>{{title}}</h1>
+            </header>
+            <main>
+                <div class="container">
+                    <div class="row">
+                        <ul>
+                            <li v-for="cards in data">
+                                <div class="card" @click="cardSearch(cards.id)">
+                                    <div class="img-cont">
+                                        <img :src="cards.cover" :alt="cards.album_name">
+                                    </div>
+                                    <div class="card-content">
+                                        <h2 class="card-title">{{cards.name}}</h2>
+                                        <h3 class="album-name">{{cards.album_name}}</h3>
+                                        <p class="score">Score: {{cards.score}}</p>
+                                        <p class="album-year">{{cards.year}}</p>
+                                    </div>
                                 </div>
-                                <div class="card-content">
-                                    <h2 class="card-title">{{card.name}}</h2>
-                                    <h3 class="album-name">{{card.album_name}}</h3>
-                                    <p class="score">{{card.score}}</p>
-                                    <p class="album-year">{{card.year}}</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+
+            </main>
+            <div class="container-pop" :class="{hide: hide}">
+                <div class="contenuto">
+                    <div class="inside"><img :src="cardData.cover" :alt="cardData.name">
+                        <div class="card-content-pop">
+                            <h2 class="card-title">{{cardData.name}}</h2>
+                            <h3 class="album-name">{{cardData.album_name}}</h3>
+                            <p class="score">Score: {{cardData.score}}</p>
+                            <p class="album-year">{{cardData.year}}</p>
+                        </div>
+                    </div>
+
+                    <div class="close" @click="showHide">X</div>
+                </div>
+
             </div>
-        </main>
-        <footer>
-            <p>Copyright||Created by <span class="creator">Draeknes</span></p>
-        </footer>
+            <footer>
+                <p>Copyright||Created by <span class="creator">Draeknes</span></p>
+            </footer>
+        </div>
     </div>
 
     <!-- vue js -->
--- main.js
+++ main.js
@@ -6,16 +6,33 @@ createApp({
       mainServerData: "http://localhost/PHP/php-dischi-json/server.php",
       title: "Best Rock/Metal Albums Ever",
       data: {},
+      cardData: {},
+      hide: true,
     };
   },
   methods: {
+    showHide() {
+      this.hide = !this.hide;
+    },
     getServerInfo() {
       axios.get(this.mainServerData).then((response) => {
         this.data = response.data;
-        console.log(this.data);
+      });
+    },
+    cardSearch(cardId) {
+      axios.get(this.mainServerData).then((response) => {
+        for (let i = 0; i < response.data.length; i++) {
+          if (response.data[i].id === cardId) {
+            this.showHide();
+            console.log(response.data[i]);
+            this.cardData = response.data[i];
+            console.log(this.cardData);
+          }
+        }
       });
     },
   },
+
   created() {
     this.getServerInfo();
   },
